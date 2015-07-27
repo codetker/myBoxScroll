fb3fd13a26b8f2527ba6d44721294437013682bb
codetker <925639912@qq.com>
2015-07-27 16:41:52 +0800

    fadeIn and fadeOut function added

--- src/jquery.codetker.boxScroll.js
+++ src/jquery.codetker.boxScroll.js
@@ -1,6 +1,6 @@
 /*
  * boxScroll 0.1
- * 兼容等常见浏览器
+ * 兼容FF,Chrome等常见浏览器
  */
 ;(function($, window, document, undefined) {
 	//定义构造函数
@@ -15,7 +15,10 @@
 			'toRight': null,
 			'ControlUl': null,
 			'circle': true,
-			'circleTime': 5
+			'circleTime': 5,
+			'fadeInTime': 300,
+			'fadeOutTime': 400,
+			'liHover': null
 		};
 
 		this.options = $.extend({}, this.defaults, opt);
@@ -28,7 +31,6 @@
 
 			var boxWindow = this.$element,
 				child = $(boxWindow).find(this.options.child),
-				stepTime = this.options.stepTime,
 				style = this.options.style,
 				circle = this.options.circle,
 				circleTime = this.options.circleTime,
@@ -36,24 +38,51 @@
 				toLeft = this.options.toLeft,
 				toRight = this.options.toRight,
 				Control = this.options.ControlUl,
+				liHover = this.options.liHover,
 				lists = $(Control).children('li'),
 				boxWidth = $(child).width(),
 				imgIndexMax = $(child).length,
 				imgIndex,
-				timer;
+				timer,
+
+				//margin and scroll stepTime
+				stepTime = this.options.stepTime,
+
+				//fadeIn and Out control time
+				fadeInTime = this.options.fadeInTime,
+				fadeOutTime = this.options.fadeOutTime,
+				startTime,
+				endTime;
 
 			//判断当前图片的位置
 			function getImgIndex() {
-				if (style === 0) { //margin
-					imgIndex = Math.round(parseInt($(boxWindow).css('margin-left')) * (-1) / boxWidth);
 
-				} else if (style === 1) { //scroll
-					imgIndex = Math.round($(boxWindow).scrollLeft() / boxWidth);
+				switch (style) {
+					case 0: //margin
+						imgIndex = Math.round(parseInt($(boxWindow).css('margin-left')) * (-1) / boxWidth);
+						break;
 
-				} else {
-					//add some more
+					case 1: //scroll
+						imgIndex = Math.round($(boxWindow).scrollLeft() / boxWidth);
+						break;
+
+					case 2: //fade
+						imgIndex = getIndexShow();
+						break;
+
+					default:
 				}
+			}
 
+			//fade 中判断图片index
+			function getIndexShow() {
+				var temp;
+				$(lists).each(function() {
+					if ($(this).hasClass(liHover)) {
+						temp = $(this).index();
+					}
+				});
+				return temp;
 			}
 
 			//设置循环
@@ -64,13 +93,20 @@
 					}, 1000 * circleTime);
 				}
 			}
-			set();
 
 			//取消循环
 			function rest() {
 				clearInterval(timer);
 				set();
 			}
+
+			//初始化
+			if (style == 2) {
+				$(child).eq(0).click();
+			}
+			set();
+			startTime = Date.parse(new Date());
+			endTime = Date.parse(new Date());
 
 			//绑定点击按钮
 			$(Control).delegate('li', 'click', function() {
@@ -80,12 +116,21 @@
 
 			//绑定左右按钮
 			$(toLeft).click(function() {
-				boxScroll(0, -1);
-				rest();
+				//主要用于防止show and hide 左右点击过快
+				startTime = Date.parse(new Date());
+				if ((startTime - endTime) > 800) {
+					boxScroll(0, -1);
+					rest();
+					endTime = Date.parse(new Date());
+				}
 			});
 			$(toRight).click(function() {
-				boxScroll(0, 1);
-				rest();
+				startTime = Date.parse(new Date());
+				if ((startTime - endTime) > 800) {
+					boxScroll(0, 1);
+					rest();
+					endTime = Date.parse(new Date());
+				}
 			});
 
 			function boxScroll(index, dir) {
@@ -113,22 +158,29 @@
 						}
 					}
 
-					lists.eq(imgIndex).addClass('liSelected');
-					lists.eq(imgIndex).siblings().removeClass('liSelected');
+					lists.eq(imgIndex).addClass(liHover);
+					lists.eq(imgIndex).siblings().removeClass(liHover);
 
 					//具体执行
-					if (style === 0) { //margin
-						$(boxWindow).animate({
-							'margin-left': imgIndex * boxWidth * (-1) + 'px'
-						}, 1000 * stepTime);
+					switch (style) {
+						case 0:
+							$(boxWindow).animate({
+								'margin-left': imgIndex * boxWidth * (-1) + 'px'
+							}, 1000 * stepTime);
+							break;
 
-					} else if (style === 1) { //scroll
-						$(boxWindow).animate({
-							'scrollLeft': imgIndex * boxWidth + 'px'
-						}, 1000 * stepTime);
+						case 1:
+							$(boxWindow).animate({
+								'scrollLeft': imgIndex * boxWidth + 'px'
+							}, 1000 * stepTime);
+							break;
 
-					} else {
+						case 2:
+							$(child).fadeOut(fadeOutTime);
+							$(child).eq(imgIndex).fadeIn(fadeInTime);
+							break;
 
+						default:
 					}
 
 				}
